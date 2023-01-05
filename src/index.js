1a709fe10d17e636229d42e82d220d75a59eea67
EladPress <presselad2@gmail.com>
2023-01-05 10:56:28 +0200

    converted to hooks in index.tsx

--- src/index.js
+++ src/index.js
@@ -24,98 +24,89 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
-//import React, { useState } from 'react';
-const React = __importStar(require("react"));
-//import ReactDOM from 'react-dom/client';
+const react_1 = require("react");
 const ReactDOM = __importStar(require("react-dom/client"));
 require("./index.css");
 const board_1 = require("./board");
 const square_1 = require("./square");
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            history: [
-                {
-                    squares: Array(9).fill(null),
-                    pos: [],
-                }
-            ],
-            stepNumber: 0,
-            xIsNext: true
-        };
+function Game(props) {
+    const [history, setHistory] = (0, react_1.useState)([
+        {
+            squares: Array(9).fill(null),
+            pos: [],
+        }
+    ]);
+    const [stepNumber, setStepNumber] = (0, react_1.useState)(0);
+    const [xIsNext, setXIsNext] = (0, react_1.useState)(true);
+    const [isReversed, setIsReversed] = (0, react_1.useState)(false);
+    function reverseClick() {
+        setIsReversed(!isReversed);
     }
-    reverseClick() {
-        this.setState({
-            isReversed: !this.state.isReversed,
-        });
-    }
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    function handleClick(i) {
+        //const history: any = history.slice(0, this.state.stepNumber + 1);
+        setHistory(history.slice(0, stepNumber + 1));
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         const pos = current.pos.slice(0, current.pos.length + 1);
         if ((0, square_1.calculateWinner)(squares) || squares[i]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? "X" : "O";
+        squares[i] = xIsNext ? "X" : "O";
         pos.push({ row: Math.floor(i / 3) + 1, col: i % 3 + 1 });
-        this.setState({
-            history: history.concat([
-                {
-                    squares: squares,
-                    pos: pos,
-                }
-            ]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext,
-            pos: i,
-        });
+        // this.setState({
+        //   history: history.concat([
+        //     {
+        //       squares: squares,
+        //       pos: pos,
+        //     }
+        //   ]),
+        //   stepNumber: history.length,
+        //   xIsNext: !xIsNext,
+        //   pos: i,
+        // });
+        setHistory(history.concat([{
+                squares: squares,
+                pos: pos,
+            }]));
+        setStepNumber(history.length);
+        setXIsNext(!xIsNext);
     }
-    jumpTo(step, buttonId) {
+    function jumpTo(step, buttonId) {
         let i;
         for (i = 0; i < 9; i++) {
             document.getElementById("square " + i).style.borderWidth = '1px';
         }
-        //console.log(this.state.history.length);
-        for (i = 0; i < this.state.history.length; i++) {
-            //console.log(`step ${i}`);
-            //console.log(i);
+        for (i = 0; i < history.length; i++) {
             document.getElementById(`step ${i}`).style.fontWeight = 'normal';
         }
         document.getElementById(buttonId).style.fontWeight = 'bold';
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0
-        });
-    }
-    render() {
         // this.setState({
-        //   isReversed: false,
+        //   stepNumber: step,
+        //   xIsNext: (step % 2) === 0
         // });
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = (0, square_1.calculateWinner)(current.squares);
-        const pos = history.pos;
-        //console.log(pos.length);
-        const moves = history.map((step, move) => {
-            const pos = step.pos[step.pos.length - 1];
-            const desc = move ?
-                'Go to move #' + move + " (" + pos["col"] + ", " + pos["row"] + ")" : //; + ", (" + col + ", " + row + ")":
-                'Go to game start';
-            const buttonId = `step ${move}`;
-            //const isActive = false;
-            return ((0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ id: buttonId, onClick: () => this.jumpTo(move, buttonId) }, { children: desc })) }, move));
-        });
-        let status;
-        if (winner) {
-            status = "Winner: " + winner;
-        }
-        else {
-            status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-        }
-        return ((0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "game" }, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "game-board" }, { children: (0, jsx_runtime_1.jsx)(board_1.Board, { squares: current.squares, onClick: (i) => this.handleClick(i) }) })), (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "game-info" }, { children: [(0, jsx_runtime_1.jsx)("div", { children: status }), (0, jsx_runtime_1.jsx)("ol", Object.assign({ style: { 'flexDirection': (this.state.isReversed ? 'column-reverse' : 'column') } }, { children: moves })), (0, jsx_runtime_1.jsx)("button", Object.assign({ onClick: () => this.reverseClick() }, { children: "reverse" }))] }))] })));
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0);
     }
+    //const history = this.state.history;
+    const current = history[stepNumber];
+    const winner = (0, square_1.calculateWinner)(current.squares);
+    const moves = history.map((step, move) => {
+        const pos = step.pos[step.pos.length - 1];
+        const desc = move ?
+            'Go to move #' + move + " (" + pos["col"] + ", " + pos["row"] + ")" : //; + ", (" + col + ", " + row + ")":
+            'Go to game start';
+        const buttonId = `step ${move}`;
+        //const isActive = false;
+        return ((0, jsx_runtime_1.jsx)("li", { children: (0, jsx_runtime_1.jsx)("button", Object.assign({ id: buttonId, onClick: () => jumpTo(move, buttonId) }, { children: desc })) }, move));
+    });
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    }
+    else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+    return ((0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "game" }, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "game-board" }, { children: (0, jsx_runtime_1.jsx)(board_1.Board, { squares: current.squares, onClick: (i) => handleClick(i) }) })), (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "game-info" }, { children: [(0, jsx_runtime_1.jsx)("div", { children: status }), (0, jsx_runtime_1.jsx)("ol", Object.assign({ style: { 'flexDirection': (isReversed ? 'column-reverse' : 'column') } }, { children: moves })), (0, jsx_runtime_1.jsx)("button", Object.assign({ onClick: () => reverseClick() }, { children: "reverse" }))] }))] })));
 }
 // ========================================
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/index.tsx
+++ src/index.tsx
@@ -1,128 +1,114 @@
-//import React, { useState } from 'react';
-import * as React from 'react';
-//import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
-import {Board} from './board'
+import { Board } from './board'
 import { calculateWinner } from './square';
 
 
+function Game(props: any) {
 
-class Game extends React.Component<any, any> {
-  
 
-  constructor(props: any) {
-    super(props);
-    
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null), 
-          pos: [],
-        }
-      ],
-      stepNumber: 0,
-      xIsNext: true
-    };
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+      pos: [],
+    }
+  ])
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [isReversed, setIsReversed] = useState(false);
+
+
+  function reverseClick() {
+    setIsReversed(!isReversed);
   }
 
-  reverseClick(){
-    this.setState({
-      isReversed: !this.state.isReversed,
-    })
-  }
-  
-  handleClick(i: number) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+
+  function handleClick(i: number) {
+    //const history: any = history.slice(0, this.state.stepNumber + 1);
+    setHistory(history.slice(0, stepNumber + 1));
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    const pos = current.pos.slice(0, current.pos.length + 1);
+    const pos: any = current.pos.slice(0, current.pos.length + 1);
 
-    if (calculateWinner(squares)|| squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    pos.push({row: Math.floor(i / 3) + 1, col: i % 3 + 1});
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares,
-          pos: pos,
-        }
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-      pos: i,
-    });
+    squares[i] = xIsNext ? "X" : "O";
+    pos.push({ row: Math.floor(i / 3) + 1, col: i % 3 + 1 });
+    
+    setHistory(history.concat([{
+      squares: squares,
+      pos: pos,
+    }]));
+    setStepNumber(history.length);
+    setXIsNext(!xIsNext);
+    
   }
 
-  jumpTo(step: any, buttonId: any) {
-    
+  function jumpTo(step: any, buttonId: any) {
+
     let i;
-    for(i = 0; i < 9; i++)
-    {
+    for (i = 0; i < 9; i++) {
       document.getElementById("square " + i)!.style.borderWidth = '1px';
     }
 
-    for(i = 0; i < this.state.history.length; i++)
-    {
+    for (i = 0; i < history.length; i++) {
       document.getElementById(`step ${i}`)!.style.fontWeight = 'normal';
     }
     document.getElementById(buttonId)!.style.fontWeight = 'bold';
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0
-    });
-  }
-
-  render() {
     // this.setState({
-    //   isReversed: false,
+    //   stepNumber: step,
+    //   xIsNext: (step % 2) === 0
     // });
-    
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    const pos = history.pos;
-    //console.log(pos.length);
-    const moves = history.map((step: { pos: string | any[]; }, move: React.Key | null | undefined) => {
-      const pos = step.pos[step.pos.length - 1];
-      const desc = move ?
-        'Go to move #' + move + " (" + pos["col"] + ", " + pos["row"] + ")"://; + ", (" + col + ", " + row + ")":
-        'Go to game start';
-      const buttonId = `step ${move}`;
-      //const isActive = false;
-      return (
-        <li key={move}>
-          <button id={buttonId} onClick={() => this.jumpTo(move, buttonId)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i: number) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol style={{'flexDirection': (this.state.isReversed ? 'column-reverse' : 'column')}}>{moves}</ol>
-          <button onClick={() => this.reverseClick()}>reverse</button>
-        </div>
-      </div>
-    );
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
   }
+
+
+
+  //const history = this.state.history;
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step: { pos: string | any[]; }, move: React.Key | null | undefined) => {
+    const pos = step.pos[step.pos.length - 1];
+    const desc = move ?
+      'Go to move #' + move + " (" + pos["col"] + ", " + pos["row"] + ")" ://; + ", (" + col + ", " + row + ")":
+      'Go to game start';
+    const buttonId = `step ${move}`;
+    //const isActive = false;
+    return (
+      <li key={move}>
+        <button id={buttonId} onClick={() => jumpTo(move, buttonId)}>{desc}</button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i: number) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol style={{ 'flexDirection': (isReversed ? 'column-reverse' : 'column') }}>{moves}</ol>
+        <button onClick={() => reverseClick()}>reverse</button>
+      </div>
+    </div>
+  );
 }
+
 
 // ========================================
 
