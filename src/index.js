fbd4a61fa1752ac21fd601de30b7a8d8c52cce98
EladPress <presselad2@gmail.com>
2023-11-28 22:20:55 +0200

    testing shit, how many commits appear?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//testing sdklfjdlk
+//testing sdklfjdlksdfsdfg
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
